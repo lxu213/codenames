a9bcac61636ba5a796aa15d3cfa7c9fc7915e014
Lily Xu <lilyxu@alum.mit.edu>
2020-02-20 20:26:31 -0800

    Add EndTurn and Gear as functions

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-// https://reactjs.org/docs/create-a-new-react-app.html#create-react-app
-// TODO: one turn may include multiple clicks
 
 const CODENAMELIST = ['Hollywood','Well','Foot','New','York','Spring','Court','Tube','Point','Tablet','Slip','Date','Drill','Lemon','Bell','Screen','Fair','Torch','State','Match','Iron','Block','France','Australia','Limousine','Stream','Glove','Nurse','Leprechaun','Play','Tooth','Arm','Bermuda','Diamond','Whale','Comic','Mammoth','Green','Pass','Missile','Paste','Drop','Pheonix','Marble','Staff','Figure','Park','Centaur','Shadow','Fish','Cotton','Egypt','Theater','Scale','Fall','Track','Force','Dinosaur','Bill','Mine','Turkey','March','Contract','Bridge','Robin','Line','Plate','Band','Fire','Bank','Boom','Cat','Shot','Suit','Chocolate','Roulette','Mercury','Moon','Net','Lawyer','Satellite','Angel','Spider','Germany','Fork','Pitch','King','Crane','Trip','Dog','Conductor','Part','Bugle','Witch','Ketchup','Press','Spine','Worm','Alps','Bond','Pan','Beijing','Racket','Cross','Seal','Aztec','Maple','Parachute','Hotel','Berry','Soldier','Ray','Post','Greece','Square','Mass','Bat','Wave','Car','Smuggler','England','Crash','Tail','Card','Horn','Capital','Fence','Deck','Buffalo','Microscope','Jet','Duck','Ring','Train','Field','Gold','Tick','Check','Queen','Strike','Kangaroo','Spike','Scientist','Engine','Shakespeare','Wind','Kid','Embassy','Robot','Note','Ground','Draft','Ham','War','Mouse','Center','Chick','China','Bolt','Spot','Piano','Pupil','Plot','Lion','Police','Head','Litter','Concert','Mug','Vacuum','Atlantis','Straw','Switch','Skyscraper','Laser','Scuba','Diver','Africa','Plastic','Dwarf','Lap','Life','Honey','Horseshoe','Unicorn','Spy','Pants','Wall','Paper','Sound','Ice','Tag','Web','Fan','Orange','Temple','Canada','Scorpion','Undertaker','Mail','Europe','Soul','Apple','Pole','Tap','Mouth','Ambulance','Dress','Ice','Cream','Rabbit','Buck','Agent','Sock','Nut','Boot','Ghost','Oil','Superhero','Code','Kiwi','Hospital','Saturn','Film','Button','Snowman','Helicopter','Loch','Ness','Log','Princess','Time','Cook','Revolution','Shoe','Mole','Spell','Grass','Washer','Game','Beat','Hole','Horse','Pirate','Link','Dance','Fly','Pit','Server','School','Lock','Brush','Pool','Star','Jam','Organ','Berlin','Face','Luck','Amazon','Cast','Gas','Club','Sink','Water','Chair','Shark','Jupiter','Copper','Jack','Platypus','Stick','Olive','Grace','Bear','Glass','Row','Pistol','London','Rock','Van','Vet','Beach','Charge','Port','Disease','Palm','Moscow','Pin','Washington','Pyramid','Opera','Casino','Pilot','String','Night','Chest','Yard','Teacher','Pumpkin','Thief','Bark','Bug','Mint','Cycle','Telescope','Calf','Air','Box','Mount','Thumb','Antarctica','Trunk','Snow','Penguin','Root','Bar','File','Hawk','Battery','Compound','Slug','Octopus','Whip','America','Ivory','Pound','Sub','Cliff','Lab','Eagle','Genius','Ship','Dice','Hood','Heart','Novel','Pipe','Himalayas','Crown','Round','India','Needle','Shop','Watch','Lead','Tie','Table','Cell','Cover','Czech','Back','Bomb','Ruler','Forest','Bottle','Space','Hook','Doctor','Ball','Bow','Degree','Rome','Plane','Giant','Nail','Dragon','Stadium','Flute','Carrot','Wake','Fighter','Model','Tokyo','Eye','Mexico','Hand','Swing','Key','Alien','Tower','Poison','Cricket','Cold','Knife','Church','Board','Cloak','Ninja','Olympus','Belt','Light','Death','Stock','Millionaire','Day','Knight','Pie','Bed','Circle','Rose','Change','Cap','Triangle'];
 const REVEALED_CLASSNAMES = ['red','red','red','red','red','red','red','red','red','blue','blue','blue','blue','blue','blue','blue','blue', 'bystander','bystander','bystander','bystander','bystander','bystander','bystander', 'assassin'];
@@ -17,6 +15,35 @@ function Card(props) {
       onClick={props.onClick}>
         {props.word}
     </button>
+  );
+}
+
+function EndTurn(props) {
+  if (props.endTurn) { // game is active
+    return (
+      <button
+        className="btn btn-info btn-light"
+        onClick={props.onClick}>
+          End Turn
+      </button>
+    )
+  } else { // game over 
+    const message = (props.winner).toUpperCase() + " TEAM WINS!"
+    return (
+      <div className={"turn col " + props.status}>{message}</div>
+    )
+  }
+}
+
+function Gear(props) {
+  return (
+    <div
+      className="gear"
+      onClick={props.onClick}>
+      <svg width="30" height="30" viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M22.3344 4.86447L24.31 8.23766C21.9171 9.80387 21.1402 12.9586 22.5981 15.4479C23.038 16.1989 23.6332 16.8067 24.3204 17.2543L22.2714 20.7527C20.6682 19.9354 18.6888 19.9151 17.0088 20.8712C15.3443 21.8185 14.3731 23.4973 14.2734 25.2596H10.3693C10.3241 24.4368 10.087 23.612 9.64099 22.8504C8.16283 20.3266 4.93593 19.4239 2.34593 20.7661L0.342913 17.3461C2.85907 15.8175 3.70246 12.5796 2.21287 10.0362C1.74415 9.23595 1.09909 8.59835 0.354399 8.14386L2.34677 4.74208C3.95677 5.5788 5.95446 5.60726 7.64791 4.64346C9.31398 3.69524 10.2854 2.0141 10.3836 0.25H14.267C14.2917 1.11932 14.5297 1.99505 15.0012 2.80013C16.4866 5.33635 19.738 6.23549 22.3344 4.86447ZM15.0038 17.3703C17.6265 15.8776 18.5279 12.5685 17.0114 9.97937C15.4963 7.39236 12.1437 6.50866 9.52304 8.00013C6.90036 9.4928 5.99896 12.8019 7.5154 15.391C9.03058 17.978 12.3832 18.8617 15.0038 17.3703Z" transform="translate(12.7548) rotate(30)" fill="#EEE" stroke="#BBB" strokeWidth="0.5"></path>          
+      </svg>
+    </div>
   );
 }
 
@@ -85,24 +112,20 @@ class Game extends React.Component {
       isRedTurn: false,
       isClueTurn: true,
       status: 'blue-turn',
-      blueRemaining: 9,
-      redRemaining: 8,
+      blueRemaining: 8,
+      redRemaining: 9,
       showEndTurn: true,
+      view: 'agent',
+      winner: '',
     };
   }
 
   handleCardClick(i) {
-    console.log('--> inside handleCardClick');
-    console.log(this.state.cardWords[i], this.state.cardClass[i])
-
-    if (this.state.status === 'game-over') {
-      return
+    if (this.state.status.includes('game-over') || this.state.view === 'spymaster') {
+      return null // disable clicking 
     }
-
     this.updateScore(i);
-
-    // set clickedCardType to cardType to switch css class
-    this.state.cardClass[i] = this.state.cardColor[i];
+    this.state.cardClass[i] = this.state.cardColor[i]; // switch css classNames
 
     if (this.state.cardColor[i] === 'bystander' ||
        (this.state.isRedTurn === true && this.state.cardColor[i] === 'blue') ||
@@ -113,27 +136,25 @@ class Game extends React.Component {
       })
     } else if (this.state.cardColor[i] === 'assassin') {
       alert('You have chosen the assassin. Game Over.')
+      const status = "game-over-" + (this.state.isRedTurn ? 'blue' : 'red');
       this.setState({
-        status: 'game-over',
+        status: status,
         showEndTurn: false,
+        winner: (this.state.isRedTurn ? 'Blue' : 'Red'),
       })
-      
-    }
+    } 
 
     this.setState({
       cardWords: this.state.cardWords,
       cardClass: this.state.cardClass,
       isClueTurn: !this.state.isClueTurn,
     });
-    console.log(this.state.cardWords[i], this.state.cardClass[i])
-    console.log('isRedTurn', this.state.isRedTurn, 'status', this.status);
-    console.log(this.state.blueRemaining, this.state.redRemaining);
   }
 
   updateScore(i) {
-    // only update score if card has not been reveald already
+    // only update score if card has not been revealed already
     if (this.state.cardClass[i] !== 'hidden-card') {
-      return
+      return null
     }
 
     // update red or blue team's score
@@ -146,6 +167,15 @@ class Game extends React.Component {
         blueRemaining: this.state.blueRemaining - 1,
       })    
     }
+    // check if game is over
+    if (this.state.redRemaining === 1 || this.state.blueRemaining === 1) {
+      const status = "game-over-" + (this.state.isRedTurn ? 'red' : 'blue');
+      this.setState({
+        status: status,
+        showEndTurn: false,
+        winner: (this.state.isRedTurn ? 'Red' : 'Blue'),
+      })
+    }
   }
 
   handleEndTurnClick(i) {
@@ -156,13 +186,18 @@ class Game extends React.Component {
   }
 
   handleSpymasterClick(i) {
-    // apply className="revealed" where the text is colored red or blue
-    // may need to learn about making a new class where css can be applied
-    console.log('spymaster')
-    console.log(this.state.cardWords[i], this.state.cardColor[i])
-    const spymasterCardNames = this.state.cardColor.map(card => "spymaster-" + card);
+    // do not map cards that aren't "hiddencard" for class
+    const spymasterCardNames = this.state.cardClass.map((card, i) => {
+      if (card === 'hidden-card') {
+        return "spymaster-" + this.state.cardColor[i]
+      } else {        
+        return card
+      }
+      
+    });
     this.setState({
       cardClass: spymasterCardNames,
+      view: 'spymaster',
     })
     // when clicked, all text should bold and 'status' is used as font-color
   }
@@ -170,8 +205,13 @@ class Game extends React.Component {
   handleAgentClick(i) {
     this.setState({
       cardClass: HIDDEN_CLASSNAMES,
+      view: 'agent',
     })
     // when clicked, all text should bold and 'status' is used as font-color
+  }
+
+  handleGearClick(i) {
+    alert('How to play codenames: https://www.youtube.com/watch?v=zQVHkl8oQEU');
   }
 
   newGame(i) {
@@ -189,7 +229,6 @@ class Game extends React.Component {
     });
     // clear input box after setting state with clue
     e.target[0].value = ""  
-    console.log('--> handleSubmit isRedTurn', this.state.isRedTurn, 'status', this.status);
   }
 
   render() {
@@ -197,7 +236,15 @@ class Game extends React.Component {
     if (this.state.status.includes('-turn')) {
       statusMessage = (this.state.isRedTurn ? 'Red' : 'Blue') + "'s Turn"
     } else {
-      statusMessage = (this.state.isRedTurn ? 'Blue' : 'Red').toUpperCase() + " TEAM WINS!"
+      statusMessage = null
+    }
+
+    let agentView;
+    let spyView;
+    if (this.state.view === 'agent') {
+      agentView = 'gray-click'
+    } else {
+      spyView = 'gray-click'
     }
 
     return (
@@ -211,11 +258,13 @@ class Game extends React.Component {
             <span className="red-turn">{this.state.redRemaining}</span>
           </div>
           <div className={"turn col " + this.state.status}>{statusMessage}</div>
-          <button
-            className="btn btn-info btn-light"
-            onClick={i => this.handleEndTurnClick(i)}>
-              {this.state.showEndTurn ? "End Turn" : null}
-          </button>
+          <EndTurn
+            onClick={i => this.handleEndTurnClick(i)}
+            endTurn={this.state.showEndTurn}
+            status={this.state.status}
+            isRedTurn={this.state.isRedTurn}
+            winner={this.state.winner}
+          />
         </div>
 
         <Board 
@@ -225,15 +274,23 @@ class Game extends React.Component {
         />
 
         <div className="info row col-12">
-          <div class="btn-group btn-group-toggle" data-toggle="buttons">
-            <label class="btn btn-info btn-light" onClick={i => this.handleAgentClick(i)}>
-              Agent
+          <Gear
+            onClick={i => this.handleGearClick(i)}
+          />
+          <div className="btn-group btn-group-toggle" data-toggle="buttons">
+            <label
+              className={"btn btn-info btn-light " + agentView} 
+              onClick={i => this.handleAgentClick(i)}>
+                Agent
             </label>
-            <label class="btn btn-info btn-light" onClick={i => this.handleSpymasterClick(i)}>
-              Spymasters
+            <label 
+              className={"btn btn-info btn-light " + spyView}
+              onClick={i => this.handleSpymasterClick(i)}>
+                Spymasters
             </label>
           </div>
-          <button className="btn btn-info btn-light new-game" onClick={i => this.newGame(i)}>
+          <button
+            className="btn btn-info btn-light new-game" onClick={i => this.newGame(i)}>
               New Game
           </button>
         </div>
@@ -292,50 +349,3 @@ function shuffle(array) {
 
   return array;
 }
-
-// function Clue(props) {
-//   return (
-//     <div 
-//       className="clue" 
-//       onSubmit={props.onSubmit}>
-//         Clue: {props.clue.toUpperCase()}
-//     </div>
-//   );
-// }
-
-
-        {/* <div className="game-info col-12">
-          <div className="clue-display">
-            <form onSubmit={e => this.handleSubmit(e)}>
-              <label>
-                Enter Clue: 
-                <input 
-                  className="clue-input" 
-                  type="text" 
-                  name="inputClue" 
-                />
-              </label>
-              <input 
-                className="clue-submit" 
-                type="submit" 
-                value="Submit" 
-              />
-            </form>
-          </div>
-          <div className="clue-display">
-            <Clue 
-              clue={this.state.clue}
-              onSubmit={e => this.handleSubmit(e)}
-            />
-          </div>
-          <div className="clue-display">
-            <div>
-              {this.state.isRedTurn ? 
-                "Red" : 
-                "Blue"}  
-              {this.state.isClueTurn ? 
-                "Spymaster, please enter your clue." : 
-                "Guesser, please click on guesses."}
-            </div>
-          </div>          
-        </div> */}
